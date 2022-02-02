c8e9f3404d65e7d15e3ab9963e5ec9b67cdd871d
oceane-dassou <oceane.dassou@outlook.fr>
2022-02-02 15:04:48 +0100

    first commit

---
+++ app/src/main/java/com/example/calcul_mental/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.calcul_mental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater= getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
