dc1168a1a308ba679333209a823eaebb34e4e571
oceane-dassou <oceane.dassou@outlook.fr>
2022-03-14 11:30:33 +0100

    logique 2

--- app/src/main/java/com/example/calcul_mental/models/CalculateModel.java
+++ app/src/main/java/com/example/calcul_mental/models/CalculateModel.java
@@ -1,4 +1,25 @@
 package com.example.calcul_mental.models;
 
 public class CalculateModel {
+    private int _firstValue;
+    private int _secondValue;
+    private String _operator;
+
+    public CalculateModel(int _firstValue, int _secondValue, String _operator){
+        this._firstValue=_firstValue;
+        this._secondValue=_secondValue;
+        this._operator=_operator;
+    }
+
+    public int getFirstValue() {
+        return _firstValue;
+    }
+
+    public int getSecondValue() {
+        return _secondValue;
+    }
+
+    public String getOperator() {
+        return _operator;
+    }
 }
